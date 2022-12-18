a83bb3430303fdb8e293777c2e5a82e6a6a0242f
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-12-18 15:13:26 +0100

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const x = "Online!"
+
+require('colors')
+
+module.exports = async client => {
+
+  console.log(x)
+
+
+ /* function status() {
+    const _status_ = [`Mr. Infinity = 😎`, `Hyro = 😎`, `Hi!`]
+    const Power = Math.floor(Math.random() * _status_.length);
+  
+     client.user.setActivity("", { type: "STREAMING" });
+  }; setInterval(status, 10000)
+
+
+
+
+
+};
+
