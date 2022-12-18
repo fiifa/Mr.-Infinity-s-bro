f3db45c8cf76f06f21362ffd368aa9c1781aad95
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-12-18 15:12:41 +0100

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const x = "😱";
+
+module.exports = async (client, message) => {
+
+  //if (message.content.toLowerCase() === "hdjdjdus7hebes87") {
+   // message.reply("🌛")
+
+
+  }
+}
